8ed557f60dc4ceea185033a36819ddae5d007753
daffy3 <weartstudio@naver.com>
2024-07-24 20:41:08 +0900

    #4 - 스프링부트 프로젝트 시작
    * 스프링 웹
    * 엑츄에이터
    * 데브툴즈
    * 롬복

---
+++ board/src/test/java/org/daffy3/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daffy3.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

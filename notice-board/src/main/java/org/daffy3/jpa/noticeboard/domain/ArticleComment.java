75c8434a17762960a50eb8ae4105c595b077134f
daffy3 <weartstudio@naver.com>
2024-08-07 23:26:21 +0900

    #15 - 도메인 클래스 설계 (게시글, 댓글)

---
+++ notice-board/src/main/java/org/daffy3/jpa/noticeboard/domain/ArticleComment.java
@@ -0,0 +1,16 @@
+package org.daffy3.jpa.noticeboard.domain;
+
+import java.time.LocalDateTime;
+
+public class ArticleComment {
+
+    private Long id;
+    private Article article; // 게시글 (id)
+    private String content; // 본문
+
+    private LocalDateTime createdAt; // 생성일시
+    private String createdBy; // 생성자
+    private LocalDateTime updatedAt; // 수정일시
+    private String updatedBy; // 수정자
+
+}
